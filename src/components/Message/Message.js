c8d55c58e4a6961a33ee97960a5eb1c21aee0d15
Inna Tishchenko <innatishchenko29@gmail.com>
2021-03-23 17:57:22 +0300

    first commit

---
+++ src/components/Message/Message.js
@@ -0,0 +1,14 @@
+import './Message.css'
+
+const Message = ({ item, currentUser }) => {
+  const classMessage = item.user === currentUser ? 'alert alert-primary' : 'alert alert-dark'
+    return (
+      <div className="message-container">
+        <span className={classMessage}>
+          {item.user}: {item.text}
+        </span>
+     </div>
+    )
+  }
+
+export default Message;
---
+++ src/components/Send/Send.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Send extends Component {
+  render() {
+    const { onChange, onSend, value } = this.props;
+    return (
+      <form className="input-group" onSubmit={onSend}>
+        <input type="text" className="form-control" value={value} onChange={onChange} />
+        <button className="btn btn-primary" type="submit">Отправить</button>
+      </form>
+    )
+  }
+}
